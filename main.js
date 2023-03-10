916ec9975c209c79a2e310a9901da8e96c3ff92d
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2023-03-10 09:56:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function ai21_rewrite(form){
 
     fetch("https://api.ai21.com/studio/v1/experimental/rewrite", {
         method: 'POST',
-        mode: "cors"
+        mode: "cors",
         headers: {
           accept: 'application/json',
           'content-type': 'application/json',
